42c872f37870ef2b602ee47bd40ab94a4d562625
Mariana da Silva Lara <marislaradev@outlook.com>
2023-10-17 15:50:02 -0300

    configurações conexão com banco de dados

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 require('dotenv').config(); //para simular variaveis de ambientes. eu posso criar um arquivo na raiz do package.json. com final .env e lá dentro eu posso criar quantas variaveis de ambientes eu quiser.
 
 const express = require('express');
+const knex = require('knex');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    return res.json('tudo ok');
+    try {
+        const carros = await knex('carros')
+        return res.json(carros)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ mensagem: 'Erro interno do servidor'})
+    }
 });
 
 const port = process.env.PORT || 3000;
